3c7e0234cf5d699d2eeeffd2b85be5a2a5b3013f
Moenen Erbuer <moenen.erbuer@ibm.com>
2024-12-10 13:32:07 +0200

    Update language

--- openad_grammar_def/grammar_def.py
+++ openad_grammar_def/grammar_def.py
@@ -1,31 +1,31 @@
 # https://pyparsing-docs.readthedocs.io/en/latest/pyparsing.html
 
+# -fmt: off
+
 import pyparsing as py
 
 #############################################
-# region - General
+# region - Strings
 
-# Optional quoted string
-quoted = py.QuotedString("'", escQuote="\\")
-opt_quoted = py.QuotedString("'", escQuote="\\") | (
-    py.Suppress(py.Literal("'")) + py.CaselessKeyword(py.printables) + py.Suppress(py.Literal("'"))
-)
-save_as = py.Optional(
-    py.Suppress(py.CaselessKeyword("save")) + py.Suppress(py.CaselessKeyword("as")) + quoted("results_file")
-)("save_as")
-
+str_any = py.Word(py.printables)
+str_strict = py.Word(py.alphanums + "-_")
+str_quoted = py.QuotedString("'", escChar="\\")
+str_strict_or_quoted = str_strict | str_quoted
+str_opt_quoted = str_quoted | (py.Suppress("'") + py.Word(py.printables) + py.Suppress("'"))
 
 # endregion
 #############################################
 # region - Lists
 
 # Open and close square brackets, with optional space on inside
-sb_open = py.Suppress(py.Literal("[") + py.Optional(py.White()))
-sb_close = py.Suppress(py.Optional(py.White()) + py.Literal("]"))
+_sb_open = py.Suppress(py.Literal("[") + py.Optional(py.White()))
+_sb_close = py.Suppress(py.Optional(py.White()) + py.Literal("]"))
 
 # Comma with optional spaces before/after
-comma = py.Suppress(py.Optional(py.White()) + py.Literal(",") + py.Optional(py.White()))
+_comma = py.Suppress(py.Optional(py.White()) + py.Literal(",") + py.Optional(py.White()))
 
+# List of quoted strings separated by commas and encapsulated in square brackets
+list_quoted = _sb_open + py.delimitedList(str_quoted, delim=",") + _sb_close
 
 # endregion
 #############################################
@@ -46,14 +46,42 @@ molecules = py.CaselessKeyword("molecules") | py.CaselessKeyword("mols")
 # molecule_identifier <<= py.Combine(py.OneOrMore(_mol_str | _mol_str_brackets))
 
 molecule_identifier = (py.Word(py.alphanums + "_[]()=,-+/\\#@.*;")) | (
-    py.Suppress(py.Literal("'")) + py.Word(py.alphanums + "_[]()=,-+/\\#@.*;") + py.Suppress(py.Literal("'"))
+    py.Suppress("'") + py.Word(py.alphanums + "_[]()=,-+/\\#@.*;") + py.Suppress("'")
 )
 
 # List of identifier strings separated by comas
-_delimited_molecule_identifiers = py.delimitedList(molecule_identifier, delim=comma)
+_delimited_molecule_identifiers = py.delimitedList(molecule_identifier, delim=_comma)
 
 # List of strings separated by comas and encapsulated in square brackets
-molecule_identifier_list = sb_open + _delimited_molecule_identifiers + sb_close
+molecule_identifier_list = _sb_open + _delimited_molecule_identifiers + _sb_close
 
+# Molecule woroking set
+molecule_working_set = py.MatchFirst(
+    [
+        py.CaselessKeyword("@mws"),
+        py.CaselessKeyword("@mols"),
+        py.CaselessKeyword("@molecules"),
+    ]
+)
+
+
+# endregion
+#############################################
+# region - Clauses
+
+# Save as <filename>
+clause_save_as = py.Optional(
+    py.Suppress(py.CaselessKeyword("save")) + py.Suppress(py.CaselessKeyword("as")) + str_quoted("results_file")
+)("save_as")
+
+# USING (param1=value, param2=value, ...)
+_using = py.CaselessKeyword("USING").suppress()
+_param_value = py.Group(str_strict_or_quoted + py.Suppress("=") + str_strict_or_quoted)
+clause_using = py.Optional(
+    _using + py.Suppress("(") + py.Optional(py.OneOrMore(_param_value))("using") + py.Suppress(")")
+)
+
+# Update molecule working set
+clause_update_mws = py.Optional(py.CaselessKeyword("update") + molecule_working_set)
 
 # endregion
